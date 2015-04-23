c5b25c8c8077c2010d8efd8ebaacd35f96e7f097
inas404 <inas404@users.noreply.github.com>
2015-04-23 16:37:33 +0200

    new counter added

--- counter.do
+++ counter.do
@@ -8,21 +8,35 @@ vsim work.updff
 # Loading work.updff(behavioral)
 # Loading work.my_dff(a_my_dff)
 add wave -position end  sim:/updff/en
+add wave -position end  sim:/updff/en2
 add wave -position end  sim:/updff/rst
 add wave -position end  sim:/updff/clk
 add wave -position end  sim:/updff/data
 add wave -position end  sim:/updff/q
 add wave -position end  sim:/updff/OVF
-force -freeze sim:/updff/en 1 0
+add wave -position end  sim:/updff/enable_all
+add wave -position end  sim:/updff/q_sig
+add wave -position end  sim:/updff/q_sigb
+# ** Warning: (vsim-WLF-5000) WLF file currently in use: vsim.wlf
+# 
+#           File in use by: Inas  Hostname: INAS-PC  ProcessID: 6044
+# 
+#           Attempting to use alternate WLF file "./wlftm79mcg".
+# ** Warning: (vsim-WLF-5001) Could not open WLF file: vsim.wlf
+# 
+#           Using alternate file: ./wlftm79mcg
+# 
+force -freeze sim:/updff/en 0 0
+force -freeze sim:/updff/en2 1 0
 force -freeze sim:/updff/rst 1 0
 force -freeze sim:/updff/clk 1 0, 0 {50 ps} -r 100
-force -freeze sim:/updff/data 4 0
+force -freeze sim:/updff/data 250 0
 run
+# ** Warning: There is an 'U'|'X'|'W'|'Z'|'-' in an arithmetic operand, the result will be 'X'(es).
+#    Time: 0 ps  Iteration: 0  Instance: /updff
+# ** Warning: There is an 'U'|'X'|'W'|'Z'|'-' in an arithmetic operand, the result will be 'X'(es).
+#    Time: 0 ps  Iteration: 0  Instance: /updff
 force -freeze sim:/updff/rst 0 0
-run
-run
-run
-run
 run
 run
 run
--- counterDFF.vhd
+++ counterDFF.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity updff is
-	port(en,rst,clk:in std_logic;
+	port(en,en2,rst,clk:in std_logic;
 	     data:in std_logic_vector(7  downto 0);
 	 	   q:inout std_logic_vector(7 downto 0);
 	     OVF:out std_logic );
@@ -14,7 +14,7 @@ port( d,clk,rst,EN : in std_logic;
 q , qinv : out std_logic);
 end component;
 
-  signal a,b,c,d,e,f,g,h,rst_sig : std_logic;
+  signal enable_all,a,b,c,d,e,f,g,h,rst_sig : std_logic;
   signal q_sig,q_sigb : std_logic_vector(7 downto 0);
   begin
   
@@ -28,33 +28,45 @@ end component;
 			h<=(q_sig(0) and q_sig(1) and q_sig(2) and q_sig(3) and q_sig(4) and q_sig(5) and q_sig(6)) xor q_sig(7);
 
 			q<= q_sig;
-      
-			rst_sig<='1' when q_sig=data 
+      enable_all<=en or en2;
+			rst_sig<='1' when (q_sig=data and en='1') or  (q_sigb=data and en2='1')
 			else rst;
 			
-
-			D1 : my_DFF port map(a,clk,rst_sig,en,q_sig(0),q_sigb(0));
-			D2 : my_DFF port map(b,clk,rst_sig,en,q_sig(1),q_sigb(1));
-			D3 : my_DFF port map(c,clk,rst_sig,en,q_sig(2),q_sigb(2));
-			D4 : my_DFF port map(d,clk,rst_sig,en,q_sig(3),q_sigb(3));
-			D5 : my_DFF port map(e,clk,rst_sig,en,q_sig(4),q_sigb(4));
-			D6 : my_DFF port map(f,clk,rst_sig,en,q_sig(5),q_sigb(5));
-			D7 : my_DFF port map(g,clk,rst_sig,en,q_sig(6),q_sigb(6));	
-			D8 : my_DFF port map(h,clk,rst_sig,en,q_sig(7),q_sigb(7));
+  
+			D1 : my_DFF port map(a,clk,rst_sig,enable_all,q_sig(0),q_sigb(0));
+			D2 : my_DFF port map(b,clk,rst_sig,enable_all,q_sig(1),q_sigb(1));
+			D3 : my_DFF port map(c,clk,rst_sig,enable_all,q_sig(2),q_sigb(2));
+			D4 : my_DFF port map(d,clk,rst_sig,enable_all,q_sig(3),q_sigb(3));
+			D5 : my_DFF port map(e,clk,rst_sig,enable_all,q_sig(4),q_sigb(4));
+			D6 : my_DFF port map(f,clk,rst_sig,enable_all,q_sig(5),q_sigb(5));
+			D7 : my_DFF port map(g,clk,rst_sig,enable_all,q_sig(6),q_sigb(6));	
+			D8 : my_DFF port map(h,clk,rst_sig,enable_all,q_sig(7),q_sigb(7));
 	
 			process(q_sig,rst,clk)
 				begin
-				if (q_sig=data and rst/='1') then
-						OVF <='1';	
-				elsif rst='1' then		
-						OVF <='0';
-				elsif rising_edge(clk) then
-						OVF <='0';
-				end if;
+				 	if (en='1')then
+					    if (q_sig=data and rst/='1') then
+							OVF <='1';	
+						elsif rst='1' then		
+							OVF <='0';
+						elsif rising_edge(clk) then
+							OVF <='0';
+						end if;
+
+					elsif (en2='1')then
+					    if (q_sigb=data and rst/='1') then
+							OVF <='1';	
+						elsif rst='1' then		
+							OVF <='0';
+						elsif rising_edge(clk) then
+							OVF <='0';
+						end if;
+					end if;
 			end process;
 
 	
 end Behavioral;
+
 
 
 
