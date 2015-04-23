be27cdf3248e965f4c5cfbb88337f87a3c41d4e5
inas404 <inashazem404@gmail.com>
2015-04-23 02:17:02 +0200

    Timer 1 tested
    Timer 1 Edited

--- counter.do
+++ counter.do
@@ -9,17 +9,19 @@ vsim work.updff
 # Loading work.my_dff(a_my_dff)
 add wave -position end  sim:/updff/en
 add wave -position end  sim:/updff/rst
-add wave -position end  sim:/updff/rst_sig
 add wave -position end  sim:/updff/clk
 add wave -position end  sim:/updff/data
 add wave -position end  sim:/updff/q
 add wave -position end  sim:/updff/OVF
-force -freeze sim:/updff/clk 1 0, 0 {50 ps} -r 100
-force -freeze sim:/updff/rst 1 0
 force -freeze sim:/updff/en 1 0
-force -freeze sim:/updff/data 00000100 0
+force -freeze sim:/updff/rst 1 0
+force -freeze sim:/updff/clk 1 0, 0 {50 ps} -r 100
+force -freeze sim:/updff/data 4 0
 run
 force -freeze sim:/updff/rst 0 0
+run
+run
+run
 run
 run
 run
--- counterDFF.vhd
+++ counterDFF.vhd
@@ -10,46 +10,49 @@ entity updff is
 end updff;
 architecture Behavioral of updff is
   component my_DFF is
-    port( d,clk,rst,EN : in std_logic;
-        	q , qinv : out std_logic);
-  end component;
-  
-  signal a,b,c,d,e,f,g,h,rst_sig : std_logic;
-  signal qb,q_sig,q_sigb : std_logic_vector(7 downto 0);
-  begin
-    process(clk,rst,EN )
-      begin
-     if  rst='0' and EN ='1'  then
-       
-			
-			a<=not q_sig(0);
-			b<=(q_sig(0) xor q_sig(1));
-			c<= (q_sig(0) and q_sig(1)and q_sigb(2)) or (q_sig(2) and (q_sigb(0) or q_sigb(1)));
-			d<=((q_sigb(0) or q_sigb(1) or q_sigb(2))and q_sig(3))or (q_sig(0) and q_sig(1) and q_sig(2) and q_sigb(3));
-			e<=(q_sig(0)and q_sig(1) and q_sig(2) and q_sig(3) and q_sigb(4)) or (q_sig(4)and (q_sigb(0) or q_sigb(1)or q_sigb(2) or q_sigb(3) ));
-			f<=(q_sig(0)and q_sig(1) and q_sig(2) and q_sig(3) and q_sig(4) and q_sigb(5)) or (q_sig(5)and (q_sigb(0) or q_sigb(1)or q_sigb(2) or q_sigb(3) or q_sigb(4)));
-			g<=(q_sig(0)and q_sig(1) and q_sig(2) and q_sig(3) and q_sig(4) and q_sig(5) and q_sigb(6)) or (q_sig(6)and (q_sigb(0) or q_sigb(1)or q_sigb(2) or q_sigb(3) or q_sigb(4) or q_sigb(5)));
-			h<=(q_sig(0)and q_sig(1) and q_sig(2) and q_sig(3) and q_sig(4) and q_sig(5) and q_sig(6) and q_sigb(7)) or (q_sig(7)and (q_sigb(0) or q_sigb(1)or q_sigb(2) or q_sigb(3) or q_sigb(4) or q_sigb(5) or q_sigb(6)));
-    
-		--else
-		--	q_sig<=(others=>'0');
-		end if;
-		if rst ='1'  then q<="00000000"; else q<=q_sig; end if;
+port( d,clk,rst,EN : in std_logic;
+q , qinv : out std_logic);
+end component;
 
-	end process; 
-	OVF<='1' when q=data else '0';
-	rst_sig<='1' when q=data else rst;--<='1' when q=data else '0';
-	--q<="00000000" when rst_sig='1'else  q_sig;
-	--q<=(others=>'0') when q_sig=data else q_sig; 
-	qb<=(others=>'1') when q_sigb= not data else q_sigb; 
-	D1 : my_DFF port map(a,clk,rst_sig,en,q_sig(0),q_sigb(0));
-	D2 : my_DFF port map(b,clk,rst_sig,en,q_sig(1),q_sigb(1));
-	D3 : my_DFF port map(c,clk,rst_sig,en,q_sig(2),q_sigb(2));
-	D4 : my_DFF port map(d,clk,rst_sig,en,q_sig(3),q_sigb(3));
-	D5 : my_DFF port map(e,clk,rst_sig,en,q_sig(4),q_sigb(4));
-	D6 : my_DFF port map(f,clk,rst_sig,en,q_sig(5),q_sigb(5));
-	D7 : my_DFF port map(g,clk,rst_sig,en,q_sig(6),q_sigb(6));	
-	D8 : my_DFF port map(h,clk,rst_sig,en,q_sig(7),q_sigb(7));
+  signal a,b,c,d,e,f,g,h,rst_sig : std_logic;
+  signal q_sig,q_sigb : std_logic_vector(7 downto 0);
+  begin
+  
+  			a<=not q_sig(0);
+			b<=(q_sig(0) xor q_sig(1));
+			c<= (q_sig(0) and q_sig(1)) xor q_sig(2);
+			d<=(q_sig(0) and q_sig(1) and q_sig(2)) xor q_sig(3);
+			e<=(q_sig(0) and q_sig(1) and q_sig(2) and q_sig(3)) xor q_sig(4);
+			f<=(q_sig(0) and q_sig(1) and q_sig(2) and q_sig(3) and q_sig(4)) xor q_sig(5);
+			g<=(q_sig(0) and q_sig(1) and q_sig(2) and q_sig(3) and q_sig(4) and q_sig(5)) xor q_sig(6);
+			h<=(q_sig(0) and q_sig(1) and q_sig(2) and q_sig(3) and q_sig(4) and q_sig(5) and q_sig(6)) xor q_sig(7);
+
+			q<= q_sig;
+      
+			rst_sig<='1' when q_sig=data 
+			else rst;
+			
+
+			D1 : my_DFF port map(a,clk,rst_sig,en,q_sig(0),q_sigb(0));
+			D2 : my_DFF port map(b,clk,rst_sig,en,q_sig(1),q_sigb(1));
+			D3 : my_DFF port map(c,clk,rst_sig,en,q_sig(2),q_sigb(2));
+			D4 : my_DFF port map(d,clk,rst_sig,en,q_sig(3),q_sigb(3));
+			D5 : my_DFF port map(e,clk,rst_sig,en,q_sig(4),q_sigb(4));
+			D6 : my_DFF port map(f,clk,rst_sig,en,q_sig(5),q_sigb(5));
+			D7 : my_DFF port map(g,clk,rst_sig,en,q_sig(6),q_sigb(6));	
+			D8 : my_DFF port map(h,clk,rst_sig,en,q_sig(7),q_sigb(7));
+	
+			process(q_sig,rst,clk)
+				begin
+				if (q_sig=data and rst/='1') then
+						OVF <='1';	
+				elsif rst='1' then		
+						OVF <='0';
+				elsif rising_edge(clk) then
+						OVF <='0';
+				end if;
+			end process;
+
 	
 end Behavioral;
 
